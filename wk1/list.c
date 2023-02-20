b30ede3667f090642292b158cbd99bffc21617fc
Jonah <jmeggs@outlook.com.au>
2023-02-20 13:58:36 +1100

    Week 1 material

---
+++ wk1/list.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct node {
+    int value;
+    struct node *next;
+};
+
+struct list {
+    struct node *head;
+};
+
+void listDelete(struct list *l, int value) {
+    if (l->head == NULL) {
+        return;
+    } else if (l->head->value == value) {
+        struct node * deleteMe = l->head;
+        l->head = l->head->next;
+        free(deleteMe);
+    } else {
+        struct node * curr = l->head;
+        while (curr->next != NULL) {
+            if (curr->next->value == value) {
+                struct node * deleteMe = curr->next;
+                curr->next = curr->next->next;
+                free(deleteMe);
+                break;
+            }
+            curr = curr->next;
+        }
+    }
+}
+
+int main(void) {
+    struct list * l = malloc(sizeof(struct list));
+    l->head = malloc(sizeof(struct node));
+    l->head->value = 1;
+    l->head->next = NULL;
+    listDelete(l, 3);
+    printf("Done!!\n");
+    return 0;
+}
